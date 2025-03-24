dbf6c317c414f8f1fe0d90a55c8fbd7023debf8b
saiprem-eng <Saiprem1225@gmail.com>
2025-03-24 21:28:02 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A mini project to implement all mlops tests and tools.',
+    author='saiprem',
+    license='',
+)
